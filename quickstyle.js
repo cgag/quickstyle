c04c6e2d01181c7d6d8638abba17764f84a5d57a
Curtis Gagliardi <cgag@riseup.net>
2014-03-09 15:28:53 -0700

    Working hand-rolled quickstyle.built.js.

---
+++ quickstyle.js
@@ -0,0 +1,39 @@
+function(jscolor) {
+  (function() {
+    var bgColorInput   = buildColorInput(changeBgColor)
+    var fontColorInput = buildColorInput(changeColor);
+    appendFirst('body', fontColorInput);
+    appendFirst('body', bgColorInput);
+
+    function appendFirst(selector, el) {
+      var container = document.querySelector(selector);
+      container.insertBefore(el, container.firstChild)
+    }
+
+    function changeBgColor() {
+      // In here, 'this' refers to the jscolor picker of the event
+      document
+        .querySelector('body')
+        .style
+        .backgroundColor = "#" + this.toString();
+    };
+
+    function changeColor() {
+      // In here, 'this' refers to the jscolor picker of the event
+      document
+        .querySelector('body')
+        .style
+        .color = "#" + this.toString();
+    };
+
+    function buildColorInput(onImmediateChange) {
+      console.log(onImmediateChange);
+      var i = document.createElement('input');
+      i.type = 'text'
+
+      var picker = new jscolor.color(i, {"onImmediateChange": onImmediateChange}); 
+
+      return i;
+    }
+  })();
+});
