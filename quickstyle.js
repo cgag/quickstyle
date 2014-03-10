1b74e8515409193e5b87b302b37c33e44715004a
Curtis Gagliardi <cgag@riseup.net>
2014-03-09 17:31:57 -0700

    Add base64 encoded data to jscolor-piece.js

--- quickstyle.js
+++ quickstyle.js
@@ -1,39 +1,38 @@
-function(jscolor) {
-  (function() {
-    var bgColorInput   = buildColorInput(changeBgColor)
-    var fontColorInput = buildColorInput(changeColor);
-    appendFirst('body', fontColorInput);
-    appendFirst('body', bgColorInput);
 
-    function appendFirst(selector, el) {
-      var container = document.querySelector(selector);
-      container.insertBefore(el, container.firstChild)
-    }
+function quickstyle(jscolor) {
+  var bgColorInput   = buildColorInput(changeBgColor)
+  var fontColorInput = buildColorInput(changeColor);
+  appendFirst('body', fontColorInput);
+  appendFirst('body', bgColorInput);
 
-    function changeBgColor() {
-      // In here, 'this' refers to the jscolor picker of the event
-      document
-        .querySelector('body')
-        .style
-        .backgroundColor = "#" + this.toString();
-    };
+  function appendFirst(selector, el) {
+    var container = document.querySelector(selector);
+    container.insertBefore(el, container.firstChild)
+  }
 
-    function changeColor() {
-      // In here, 'this' refers to the jscolor picker of the event
-      document
-        .querySelector('body')
-        .style
-        .color = "#" + this.toString();
-    };
+  function changeBgColor() {
+    // In here, 'this' refers to the jscolor picker of the event
+    document
+    .querySelector('body')
+    .style
+    .backgroundColor = "#" + this.toString();
+  };
 
-    function buildColorInput(onImmediateChange) {
-      console.log(onImmediateChange);
-      var i = document.createElement('input');
-      i.type = 'text'
+  function changeColor() {
+    // In here, 'this' refers to the jscolor picker of the event
+    document
+      .querySelector('body')
+      .style
+      .color = "#" + this.toString();
+  };
+
+  function buildColorInput(onImmediateChange) {
+    console.log(onImmediateChange);
+    var i = document.createElement('input');
+    i.type = 'text'
 
       var picker = new jscolor.color(i, {"onImmediateChange": onImmediateChange}); 
 
-      return i;
-    }
-  })();
-});
+    return i;
+  }
+};
